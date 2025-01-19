14711930593ae95dad22bea595d76595dc7f45e2
Mike Spears <mws@samesimilar.com>
2025-01-19 01:54:03 -0500

    added readme files and documentation

---
+++ README.md
@@ -0,0 +1,42 @@
+# A Browser-Based OLED emulator for Organelle
+This project contains a system of components for running [Organelle](https://www.critterandguitari.com/organelle) patches on a standard desktop/laptop operating system (I've tested it on macOS). 
+
+![img](screenshot1.png)
+
+It has 3 main components:
+
+* A slightly modified version of `mother.pd` that provides the runtime environment needed by Organelle patches in PureData.
+* A browser-based simulator (`index.html`) of the Organelle's OLED panel and hardware controls.
+* A python script (`oscweb.py`) to pass OSC messages from PureData to a web browser by broadcasting them over a web socket.
+
+## Installing
+
+The python script has two required dependencies: 
+* websockets 14.1
+* python-osc 1.8
+
+These can be installed in a 'virtual environment' so they are kept locally with this project.
+
+To Install (tested on macOS):
+
+1. First - you need to have Python 3 and pip installed on your system. See [python.org](https://python.org) for details.	
+	* You can try running `python3` in a Terminal window to see if it is installed.
+
+Run the following commands:
+
+2. Open a terminal and change to the project directory: `cd /path/to/organelle-oled/project` 
+3. create virtual environment: `python3 -m venv .venv`
+	* This creates a `.venv` folder inside the current (project) folder to hold the dependencies that will be installed in step 5.
+4. activate virtual environment: `source .venv/bin/activate`
+5. install the required dependencies (websockets and python-osc): `python3 -m pip install -r requirements.txt`
+
+## Starting up
+
+To use an Organelle patch, start the components of this system in the following order:
+
+1. With the virtual environment activated in the Terminal (Step 4, above), run `python3 oscweb.py`
+2. Open `index.html` in Safari or Chrome. (e.g. Double-click this project's `index.html` file in the Finder on macOS.)
+3. Open `mother.pd` in Pd.
+4. Open the Organelle patch you wish to edit or test in Pd.
+
+If your browser is not connected to the web socket, make sure `oscweb.py` is running, and reload `index.html`.
--- oscweb.py
+++ oscweb.py
@@ -1,21 +1,42 @@
 #!python3
-# installing:
+#
+# oscweb.py
+# Copyright 2025 Michael Spears (mws@samesimilar.com)
+# See LICENSE.
+#
+# Enables communication between OSC (https://opensoundcontrol.org) and a web browser via a WebSocket (https://websockets.spec.whatwg.org)
+#
+# web browser <---> [websocket port 8002] <---> oscweb [OSC protocol] <--- Pd net send [port 4002]  e.g. Pd (puredata.info)
+#                                                                    |---> Pd net receive [port 4000]
+# Installing:
+# This method isolates the package dependencies in a local virtual environment.
 # https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/#create-and-use-virtual-environments
+# - `cd /path/to/project`
 # - create virtual environment: `python3 -m venv .venv`
 # - activate virtual environment: `source .venv/bin/activate`
 # - install requirements: `python3 -m pip install -r requirements.txt`
+# - run: `python3 oscweb.py`
+#
+# Default Ports (These details can be changed in the variables below.)
+# 
+# - Pd should receive incoming OSC messages on UDP port 4000.
+# - PD should send outgoing OSC messages to 127.0.0.1 on UDP port 4002.
+# - The browser should connect to the WebSocket server on port 8002.
+
 
 
 
 import asyncio
 import json
-from websockets.server import broadcast, serve
-from pythonosc import udp_client
 import base64
 
+from websockets.asyncio.server import broadcast, serve
+
+from pythonosc import udp_client
 from pythonosc.dispatcher import Dispatcher
 from pythonosc.osc_server import AsyncIOOSCUDPServer
 
+# Connection sites:
 internal_osc_pd_side_ip = "127.0.0.1"
 internal_osc_python_side_ip = "127.0.0.1"
 internal_osc_pd_side_port = 4000
@@ -70,7 +91,10 @@ async def main():
 	print(f"Created OSC server to listen to Pd at {internal_osc_python_side_ip}:{internal_osc_python_side_port}")
 	async with serve(echo, external_websocket_interface_ip, external_websocket_port):
 		print(f"Created WebSocket server on {external_websocket_interface_ip}:{external_websocket_port}")
+		print("")
+		print("Leave this script running and open index.html in browser window. Press ctrl+c to stop the server.")
 		await asyncio.get_running_loop().create_future()  # run forever
 
 if __name__ == "__main__":
 	asyncio.run(main())
+	
